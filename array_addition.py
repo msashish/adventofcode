8532106645a1af79e2fb8940201382756df2ada3
Ashish Sheelavantar <ashish.sheelavantar@anz.com>
2020-04-27 19:29:54 +0530

    Added add_by factor as it need not always be 1

--- array_addition.py
+++ array_addition.py
@@ -2,18 +2,18 @@
 
 class ArrayAddition:
 
-    def get_output_array(self, input_list):
+    def get_added_array(self, input_list, add_by=1):
         output_list = [0] * len(input_list)
-        carry_over = 1
+        carry_over = add_by
         for pos, num in reversed(list(enumerate(input_list))):
             temp = num + carry_over
             if temp > 9:
-                output_list[pos] = 0
-                carry_over = 1
+                output_list[pos] = temp % 10
+                carry_over = temp // 10
             else:
                 output_list[pos] = temp
                 carry_over = 0
         if carry_over == 1:
             output_list = [1] + output_list
-        print(output_list)
+        print(f"Input array = {input_list} Output array = {output_list}")
         return output_list
--- tests/test_array_addition.py
+++ tests/test_array_addition.py
@@ -13,9 +13,16 @@ class TestArrayAddition(unittest.TestCase):
 
     def test_correct_array_addition(self):
         print('running correct_array_addition')
-        self.assertNotEqual(self.aa.get_output_array(self.array1), [5])
-        self.assertEqual(self.aa.get_output_array(self.array1), [5, 6, 7, 9])
+        self.assertNotEqual(self.aa.get_added_array(self.array1), [5])
+        self.assertEqual(self.aa.get_added_array(self.array1), [5, 6, 7, 9])
 
     def test_carry_over_addition(self):
-        self.assertEqual(self.aa.get_output_array(self.array2), [1, 0, 0, 0, 0])
-        self.assertEqual(self.aa.get_output_array(self.array3), [1, 0, 0, 0, 0, 0, 0, 0])
+        print('running test_carry_over_addition')
+        self.assertEqual(self.aa.get_added_array(self.array2), [1, 0, 0, 0, 0])
+        self.assertEqual(self.aa.get_added_array(self.array3), [1, 0, 0, 0, 0, 0, 0, 0])
+
+    def test_add_by_addition(self):
+        print('running add_by_addition')
+        self.assertEqual(self.aa.get_added_array(self.array1, add_by=3), [5, 6, 8, 1])
+        self.assertEqual(self.aa.get_added_array(self.array2, add_by=130), [1, 0, 1, 2, 9])
+        self.assertEqual(self.aa.get_added_array(self.array3, add_by=5), [1, 0, 0, 0, 0, 0, 0, 4])
